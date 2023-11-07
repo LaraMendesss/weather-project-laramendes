3bf7a6f0a7d538c6e66849e0adc19931f2cbde50
LaraMendesss <laraicmendes@gmail.com>
2023-11-07 18:10:26 -0500

    adding javascript form

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function city(event){
+    event.preventdefault();
+    let searchInput = document.querySelector("#search-text-input");
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = searchInput.value;
+}
+
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", city);
+
+let button = document.querySelector("#find");
+button.addEventListener("click",city);
