22aa66934687488e097dd5693a94b3274b9919bb
LaraMendesss <laraicmendes@gmail.com>
2023-11-07 18:24:50 -0500

    updated javascript

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 
-function city(event){
+
+
+function cityFind(event) {
     event.preventdefault();
     let searchInput = document.querySelector("#search-text-input");
     let h1 = document.querySelector("h1");
+
     h1.innerHTML = searchInput.value;
+
+   
 }
 
 
-let form = document.querySelector("#form");
+let form = document.querySelector("form");
 form.addEventListener("submit", city);
 
 let button = document.querySelector("#find");
-button.addEventListener("click",city);
+button.addEventListener("click",cityFind);
