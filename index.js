7c24da1f9d9dd1dfc8a8a680ed29a570c6978658
LaraMendesss <laraicmendes@gmail.com>
2023-11-08 11:08:08 -0500

    Added Axios

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 
 
 function cityFind(event) {
-    event.preventdefault();
+    event.preventDefault();
     let searchInput = document.querySelector("#search-text-input");
     let h1 = document.querySelector("h1");
-
     h1.innerHTML = searchInput.value;
-
-   
 }
 
 
 let form = document.querySelector("form");
-form.addEventListener("submit", city);
+form.addEventListener("submit", cityFind);
 
 let button = document.querySelector("#find");
 button.addEventListener("click",cityFind);
+
+function showTemp(response){
+  let temperature=response.data.temperature.current;
+
+console.log(response);
+
+}
+
+let searchInput = document.querySelector("#search-text-input");
+
+let city=searchInput.value;
+let apiKey= "dda9a648t200432eo3334f85db57e348";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+//console.log(apiUrl);
+
+axios.get(apiUrl).then(showTemp);
+
+
+
